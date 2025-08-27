812e4b40e66a08cd295107ca84c8ff24d718937b
Magomedhan Abdulaev <abdulaevham@gmail.com>
2025-08-28 00:43:58 +0300

    added cors origin

--- server/src/main.ts
+++ server/src/main.ts
@@ -15,7 +15,11 @@ async function bootstrap() {
 
   // CORS
   app.enableCors({
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
+    origin: [
+      "http://localhost:3000",
+      "http://127.0.0.1:3000",
+      "https://dystore.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
     credentials: true,
