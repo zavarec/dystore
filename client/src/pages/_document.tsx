e2baee4bec9c1872e8c74076eb4599db44a0e79f
Magomedhan Abdulaev <abdulaevham@gmail.com>
2025-10-17 22:35:29 +0300

    added y-metrika script

--- client/src/pages/_document.tsx
+++ client/src/pages/_document.tsx
@@ -1,14 +1,10 @@
-import React from 'react';
-import Document, {
-  Html,
-  Head,
-  Main,
-  NextScript,
-  DocumentContext,
-  DocumentInitialProps,
-} from 'next/document';
+import type { DocumentContext, DocumentInitialProps } from 'next/document';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
 import createEmotionServer from '@emotion/server/create-instance';
+
 import { createEmotionCache } from '@/utils/emotion';
+import { AppType } from 'next/app';
 
 interface MyDocumentProps extends DocumentInitialProps {
   emotionStyleTags: JSX.Element[];
@@ -59,6 +55,42 @@ export default class MyDocument extends Document<MyDocumentProps> {
           <link rel="dns-prefetch" href="//fonts.googleapis.com" />
           <link rel="dns-prefetch" href="//fonts.gstatic.com" />
           <link rel="icon" href="/favicon.ico?v=2" />
+
+          <script
+            type="text/javascript"
+            dangerouslySetInnerHTML={{
+              __html: `
+    (
+      function(m,e,t,r,i,k,a){
+        m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+        m[i].l=1*new Date();
+        for (var j = 0; j < document.scripts.length; j++) {
+          if (document.scripts[j].src === r) { 
+            return; 
+          }}
+        k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)
+    })(window, document,'script','https://mc.yandex.ru/metrika/tag.js?id=104687310', 'ym');
+
+    ym(104687310, 'init', {
+    ssr:true, webvisor:true, clickmap:true, ecommerce:"dataLayer", accurateTrackBounce:true, trackLinks:true
+    })
+  `,
+            }}
+          >
+            ;
+          </script>
+          <noscript>
+            <div>
+              <img
+                src="https://mc.yandex.ru/watch/104687310"
+                style={{
+                  position: 'absolute',
+                  left: '-9999px',
+                }}
+                alt=""
+              />
+            </div>
+          </noscript>
         </Head>
         <body>
           <Main />
